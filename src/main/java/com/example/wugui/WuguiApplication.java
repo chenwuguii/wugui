58bff67af43ff76c668543547780733061d6719a
wugui <chenwuguii@163.com>
2020-12-21 11:30:35 +0800

    创建项目

---
+++ src/main/java/com/example/wugui/WuguiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wugui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuguiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuguiApplication.class, args);
+    }
+
+}
